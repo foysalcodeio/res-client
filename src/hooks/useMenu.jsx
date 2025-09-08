5d1a76c7fd2eaeec04b7209ee8de7a0fb006795e
foysalcodeio <md.foysalahammedforhad@gmail.com>
2025-09-08 22:55:26 +0600

    working on useMenu

--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -1,27 +1,39 @@
-import { useEffect, useState } from "react";
+//import { useEffect, useState } from "react";
+
+import { useQuery } from "@tanstack/react-query";
+import useAxiosPublic from "./useAxiosPublic";
 
 const useMenu = () => {
-    const [menu, setMenu] = useState([]);
-    const [loading, setLoading] = useState(true);
+    // const [menu, setMenu] = useState([]);
+    // const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        fetch('http://localhost:5000/menu')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                }
-                return response.json(); // ✅ Remove extra .json() call
-            })
-            .then(data => {
-                setMenu(data);
-                setLoading(false);
-            })
-            .catch(error => {
-                console.error("Error:", error);
-            });
-    }, []);
+    // useEffect(() => {
+    //     fetch('http://localhost:5000/menu')
+    //         .then(response => {
+    //             if (!response.ok) {
+    //                 throw new Error(`HTTP error! status: ${response.status}`);
+    //             }
+    //             return response.json(); // ✅ Remove extra .json() call
+    //         })
+    //         .then(data => {
+    //             setMenu(data);
+    //             setLoading(false);
+    //         })
+    //         .catch(error => {
+    //             console.error("Error:", error);
+    //         });
+    // }, []);
 
-    return [menu, loading];
+    const axiosPublic = useAxiosPublic();
+
+    const {data: menu = [], isPending: loading, refetch} = useQuery({
+        queryKey: ['menu'],
+        queryFn: async () => {
+            const res = await axiosPublic.get('/menu');
+            return res.data;
+        }
+    })
+    return [menu, loading, refetch];
 };
 
 export default useMenu;
